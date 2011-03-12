eef6273629afe8882c4790b7d40ff758a863a224
Miguel Garcia <miguel.garcia@gmail.com>
2011-03-12 20:09:14 -0300

    contenido static

--- meregistro/shortcuts.py
+++ meregistro/shortcuts.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from django.conf import settings
 
 def my_render(request, template, context):
   context.update(csrf(request))
+  context['STATIC_URL'] = settings.STATIC_URL
   return render_to_response(template, context)  
--- meregistro/urls.py
+++ meregistro/urls.py
@@ -1,23 +1,10 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^login', 'seguridad.views.login', name='login'),
     url(r'^seleccionarPerfil', 'seguridad.views.seleccionarPerfil', name='seleccionarPerfil'),
-    # Example:
-    # (r'^meregistro/', include('meregistro.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'^' + settings.STATIC_URL_PATH + '/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_DOC_ROOT}),
 )
-
-
-
-
 
