9f9f274eba8a056a226ec45a5b1184835c9a77a3
Miguel Garcia <miguel.garcia@gmail.com>
2011-03-19 19:53:22 -0300

    Cambios en el modelo y datos de prueba

--- meregistro/seguridad/models/Ambito.py
+++ meregistro/seguridad/models/Ambito.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Ambito(models.Model):
   descripcion = models.CharField(max_length=255)
-  path = models.CharField(max_length=255)
+  parent = models.ForeignKey('self', null=True)
   level = models.IntegerField()
 
   class Meta:
