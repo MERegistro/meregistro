b5a418fff6d4a99e6be7fd7cce2406c00741d5b3
Miguel Garcia <miguel.garcia@gmail.com>
2011-03-22 01:06:13 -0300

    Validacion en alta de usuario

--- meregistro/seguridad/forms/UsuarioCreateForm.py
+++ meregistro/seguridad/forms/UsuarioCreateForm.py
@@ -12,3 +12,8 @@ class UsuarioCreateForm(ModelForm):
   repeat_password = forms.CharField(widget=forms.PasswordInput(render_value=False), label='Repetir contraseña')
   rol = forms.ModelChoiceField(queryset=Rol.objects, label='rol')
   ambito = forms.ModelChoiceField(queryset=Ambito.objects, label='ambito')
+
+  def clean(self):
+    if self.cleaned_data.get('password') != self.cleaned_data.get('repeat_password'):
+	    self._errors['password'] = self.error_class([u'Las contraseñas no coinciden'])
+    return self.cleaned_data
