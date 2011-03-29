c201f822b7ec81df619886d11fe96804c8841037
Luciano <luciano.baraglia[at]gmail.com>
2011-03-29 16:49:09 -0300

    Se quita la relación de Establecimiento con Jurisdicción

--- meregistro/registro/models/Establecimiento.py
+++ meregistro/registro/models/Establecimiento.py
@@ -1,12 +1,13 @@
 from django.db import models
-from meregistro.registro.models import DependenciaFuncional
+from meregistro.registro.models import DependenciaFuncional, Jurisdiccion
 
 class Establecimiento(models.Model):
   dependencia_funcional = models.ForeignKey(DependenciaFuncional)
   cue = models.IntegerField(unique=True)
   nombre = models.CharField(max_length=100)
-  norma_creacion = models.CharField(max_length=100)
-  obs = models.CharField(max_length=255)
-    
+  norma_creacion = models.CharField(max_length=100, null=True)
+  observaciones = models.CharField(max_length=255)
+  anio_creacion = models.IntegerField(null = True)
+
   class Meta:
     app_label = 'registro'
