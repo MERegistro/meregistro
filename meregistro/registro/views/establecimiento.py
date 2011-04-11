4c9003b4aa872e96aad1bb32c9e24ecf06a50059
Luciano <luciano.baraglia[at]gmail.com>
2011-04-11 01:38:11 -0300

    Paginador en establecimiento - Chequear que se pierden los filtros

--- meregistro/registro/views/establecimiento.py
+++ meregistro/registro/views/establecimiento.py
@@ -8,24 +8,47 @@ from seguridad.decorators import login_required
 from seguridad.models import Usuario, Perfil
 from registro.models import Establecimiento, Estado
 from registro.forms import EstablecimientoFormFilters, EstablecimientoForm
+from django.core.paginator import Paginator
 
+ITEMS_PER_PAGE = 40
 
 @login_required
 def index(request):
-  """
-  Búsqueda de establecimientos
-  """
-  if request.method == 'POST':
-    form_filter = EstablecimientoFormFilters(request.POST)
-#    if form_filter.is_valid():
-#      q = build_query(form_filter)
-  else:
-    form_filter = EstablecimientoFormFilters()
-  q = build_query(form_filter, 1)
-  return my_render(request, 'registro/establecimiento/index.html', {
-    'form_filters': form_filter,
-    'objects': q
-  })
+	"""
+	Búsqueda de establecimientos
+	"""
+	if request.method == 'POST':
+		form_filter = EstablecimientoFormFilters(request.POST)
+	else:
+		form_filter = EstablecimientoFormFilters()
+	q = build_query(form_filter, 1)
+
+	paginator = Paginator(q, ITEMS_PER_PAGE)
+
+	try:
+		page_number = int(request.GET['page']) # page es un int?
+	except (KeyError, ValueError):
+		page_number = 1
+	# chequear los límites
+	if page_number < 1:
+		page_number = 1
+	elif page_number > paginator.num_pages:
+		page_number = paginator.num_pages
+
+	page = paginator.page(page_number)
+	objects = page.object_list
+	return my_render(request, 'registro/establecimiento/index.html', {
+		'form_filters': form_filter,
+		'objects': objects,
+		'show_paginator': paginator.num_pages > 1,
+		'has_prev': page.has_previous(),
+		'has_next': page.has_next(),
+		'page': page_number,
+		'pages': paginator.num_pages,
+		'pages_range': range(1, paginator.num_pages + 1),
+		'next_page': page_number + 1,
+		'prev_page': page_number - 1
+	})
 
 def build_query(filters, page):
   """
@@ -56,6 +79,7 @@ def create(request):
 
   return my_render(request, 'registro/establecimiento/new.html', {
     'form': form,
+    'is_new': True,
   })
 
 @login_required
