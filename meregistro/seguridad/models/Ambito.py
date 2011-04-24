195714481d3185a607a4125ee86094be3b59031c
Miguel Garcia <miguel.garcia@gmail.com>
2011-04-24 01:59:14 -0300

    arbol de ambitos

--- meregistro/seguridad/models/Ambito.py
+++ meregistro/seguridad/models/Ambito.py
@@ -4,11 +4,21 @@ from django.db import models
 
 class Ambito(models.Model):
   descripcion = models.CharField(max_length=255)
-  parent = models.ForeignKey('self', null=True)
+  parentPath = models.CharField(max_length=255)
   level = models.IntegerField()
+  parent = models.ForeignKey('self', null=True)
 
   class Meta:
     app_label = 'seguridad'
 
   def __unicode__(self):
     return self.descripcion
+
+  def createChild(self, childDescripcion):
+    child = Ambito()
+    child.descripcion = childDescripcion
+    child.level = self.level + 1
+    child.parentPath = self.parentPath + str(self.id) + '/'
+    child.parent = self
+    child.save()
+    return child
