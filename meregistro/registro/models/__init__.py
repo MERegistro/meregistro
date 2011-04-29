6c6ca9123c341348b08e4c33b7ecc6751750f98d
Luciano <luciano.baraglia[at]gmail.com>
2011-04-29 01:59:21 -0300

    Se agregan clases para cargar automaticamente en el __ini__

--- meregistro/registro/models/__init__.py
+++ meregistro/registro/models/__init__.py
@@ -9,8 +9,8 @@ from TipoDependenciaFuncional import TipoDependenciaFuncional
 from TipoDominio import TipoDominio
 from DependenciaFuncional import DependenciaFuncional
 from Establecimiento import Establecimiento
-from Anexo import Anexo
-from Turno import Turno
 from Estado import Estado
+from Turno import Turno
 from Funcion import Funcion
 from Nivel import Nivel
+from Anexo import Anexo
