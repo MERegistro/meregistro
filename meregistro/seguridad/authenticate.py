fb850722971b297c04964843dbb77969f70e774a
Hector Sanchez <hsanchez@pressenter.com.ar>
2011-05-21 00:45:27 -0300

    identando y quitando parametros e imports sin uso

--- meregistro/seguridad/authenticate.py
+++ meregistro/seguridad/authenticate.py
@@ -1,31 +1,30 @@
 from hashlib import sha1
 from seguridad.models import Usuario
 
-def encrypt_password(usuario, password):
-  """
-  Encripta una password para el Usuario usuario.
+def encrypt_password(password):
+    """
+    Encripta una password para el Usuario usuario.
 
-  @param Usuario usuario.
-  @param string password.
-  @return string password encriptada.
-  """
-  return sha1(password).hexdigest()
+    @param Usuario usuario.
+    @param string password.
+    @return string password encriptada.
+    """
+    return sha1(password).hexdigest()
 
 def authenticate(tipo_documento, documento, password):
-  """
-  Autentica un usuario a partir de sus credenciales de acceso
-  
-  @param TipoDocumento tipo de documento del usuario.
-  @param string documento documento del usuario.
-  @param string password password del usuario.
-  @return Usuario|boolean False si las credenciales de acceso no son validas o
-  el objeto Usuario en caso que si lo sean.
-  """
-  try:
-    user = Usuario.objects.get(tipo_documento=tipo_documento, documento=documento)
-    if user.password == encrypt_password(user, password):
-      return user
-    return False
-  except Usuario.DoesNotExist:
-    return False
-  
+    """
+    Autentica un usuario a partir de sus credenciales de acceso
+
+    @param TipoDocumento tipo de documento del usuario.
+    @param string documento documento del usuario.
+    @param string password password del usuario.
+    @return Usuario|boolean False si las credenciales de acceso no son validas o
+    el objeto Usuario en caso que si lo sean.
+    """
+    try:
+        user = Usuario.objects.get(tipo_documento=tipo_documento, documento=documento)
+        if user.password == encrypt_password(user, password):
+            return user
+        return False
+    except Usuario.DoesNotExist:
+        return False
--- meregistro/seguridad/decorators.py
+++ meregistro/seguridad/decorators.py
@@ -1,28 +1,28 @@
-from functools import update_wrapper, wraps
+from functools import wraps
 from django.http import HttpResponseRedirect
 
 def login_required(view):
-  """
-  Chequea que el usuario este logueado.
-  """
-  @wraps(view)
-  def wrapper(request, *args, **kwargs):
-    if not request.user.is_authenticated():
-      return HttpResponseRedirect('/login?login_required')
-    return view(request, *args, **kwargs)
-  return wrapper
-
-def credential_required(credential):
-  """
-  Chequea que el usuario este logueado y el rol del perfil que selecciono
-  tenga la credencial credential.
-  """
-  def check_credential(view):
-    @login_required
+    """
+    Chequea que el usuario este logueado.
+    """
     @wraps(view)
     def wrapper(request, *args, **kwargs):
-      if credential not in request.get_credenciales():
-        return HttpResponseRedirect('/secure')
-      return view(request, *args, **kwargs)
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect('/login?login_required')
+        return view(request, *args, **kwargs)
     return wrapper
-  return check_credential
+
+def credential_required(credential):
+    """
+    Chequea que el usuario este logueado y el rol del perfil que selecciono
+    tenga la credencial credential.
+    """
+    def check_credential(view):
+        @login_required
+        @wraps(view)
+        def wrapper(request, *args, **kwargs):
+            if credential not in request.get_credenciales():
+                return HttpResponseRedirect('/secure')
+            return view(request, *args, **kwargs)
+        return wrapper
+    return check_credential
