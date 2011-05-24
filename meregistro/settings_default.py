2a6880298985364fca5eedc367107379a0bba93c
Luciano <luciano.baraglia[at]gmail.com>
2011-05-23 22:01:51 -0300

    Comentada la configuración de la DB y STATIC_URL, que dependen de la configuración local

--- meregistro/settings_default.py
+++ meregistro/settings_default.py
@@ -12,6 +12,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -22,6 +23,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+"""
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -106,5 +108,5 @@ INSTALLED_APPS = (
 )
 
 STATIC_URL_PATH = 'static'
-STATIC_URL = 'http://localhost:8080/' + STATIC_URL_PATH + '/'
+#STATIC_URL = 'http://localhost:8080/' + STATIC_URL_PATH + '/'
 STATIC_DOC_ROOT = os.path.join(PROJECT_ROOT, 'static')
