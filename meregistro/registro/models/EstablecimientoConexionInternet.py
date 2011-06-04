ff0b3d1ad100845f56b072e4127d330e31261551
Luciano <luciano.baraglia[at]gmail.com>
2011-06-04 13:54:04 -0300

    ABM de conexión a internet del establecimiento - CU 26

---
+++ meregistro/registro/models/EstablecimientoConexionInternet.py
@@ -0,0 +1,20 @@
+from django.db import models
+from registro.models.TipoConexion import TipoConexion
+from registro.models.Establecimiento import Establecimiento
+
+
+class EstablecimientoConexionInternet(models.Model):
+    establecimiento = models.ForeignKey('Establecimiento')
+    tipo_conexion = models.ForeignKey('TipoConexion')
+    proveedor = models.CharField(max_length=30)
+    tiene_conexion = models.BooleanField()
+    costo = models.DecimalField(max_digits=12, decimal_places=2)
+    cantidad = models.IntegerField()
+
+    class Meta:
+        app_label = 'registro'
+        db_table = 'registro_establecimiento_conexion_internet'
+        ordering = ['proveedor']
+
+    def __unicode__(self):
+        return self.descripcion
