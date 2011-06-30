1867a8e0db25c4a79de97185b6765c188db1d97f
Luciano <luciano.baraglia[at]gmail.com>
2011-06-30 11:25:13 -0300

    Se agregan las urls de títulos

--- meregistro/urls.py
+++ meregistro/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 import seguridad.urls
 import registro.urls
+import titulos.urls
 
 urlpatterns = patterns('',
     url(r'^login', 'seguridad.views.login', name='login'),
     url(r'^logout', 'seguridad.views.logout', name='logout'),
     url(r'^seguridad/', include(seguridad.urls)),
     url(r'^registro/', include(registro.urls)),
+    url(r'^titulos/', include(titulos.urls)),
     url(r'^seleccionarPerfil', 'seguridad.views.seleccionar_perfil', name='seleccionarPerfil'),
     url(r'^' + settings.STATIC_URL_PATH + '/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_DOC_ROOT}),
