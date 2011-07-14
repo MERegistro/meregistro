3da73d3424b7e563cf68a5acdabda442b1b738a1
Luciano <luciano.baraglia[at]gmail.com>
2011-07-14 12:44:59 -0300

    Modificación en el acceso a los estados

--- meregistro/apps/registro/models/Anexo.py
+++ meregistro/apps/registro/models/Anexo.py
@@ -58,7 +58,7 @@ class Anexo(models.Model):
 
     def getEstadoActual(self):
         try:
-            return list(self.estados)[-1]
+            return list(self.estados)[-1].estado
         except IndexError:
             return None
 
