13b0ec0f5d6266459a8d293aebf549f03bda4c78
Luciano <luciano.baraglia[at]gmail.com>
2011-07-30 00:31:11 -0300

    Pequeñas modificaciones

--- meregistro/apps/registro/forms/EstablecimientoCambiarEstadoForm.py
+++ meregistro/apps/registro/forms/EstablecimientoCambiarEstadoForm.py
@@ -4,5 +4,5 @@ from apps.registro.models.Estado import Estado
 
 
 class EstablecimientoCambiarEstadoForm(forms.Form):
-    estado = forms.ModelChoiceField(queryset=Estado.objects, label='estado')
-    observaciones = forms.CharField(required=False, widget=forms.Textarea)
+    estado = forms.ModelChoiceField(queryset = Estado.objects, label = 'estado')
+    observaciones = forms.CharField(required = False, widget = forms.Textarea)
