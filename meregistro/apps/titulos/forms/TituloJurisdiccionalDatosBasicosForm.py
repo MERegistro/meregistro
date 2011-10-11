ee05cc864233b6be19603f6bdf69909e348742b5
Luciano <luciano.baraglia[at]gmail.com>
2011-10-11 02:00:21 -0300

    Extraño error - Revirtiendo

--- meregistro/apps/titulos/forms/TituloJurisdiccionalDatosBasicosForm.py
+++ meregistro/apps/titulos/forms/TituloJurisdiccionalDatosBasicosForm.py
@@ -3,11 +3,12 @@ from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from django import forms
 from apps.titulos.models import TituloJurisdiccional
+from apps.titulos.models import Titulo
 from apps.titulos.forms import TituloJurisdiccionalForm
 
 
 class TituloJurisdiccionalDatosBasicosForm(forms.ModelForm):
-    titulo = forms.ChoiceField(label = 'Título', choices = [], required = True)
+    #titulo = forms.ModelChoiceField(label = 'Título', choices = choices, required = True)  -> No funciona, arroja que la opción elegida no es válida
 
     class Meta:
        model = TituloJurisdiccional
