83f1ab850304ec1b12b66e32c4ae1dbaba843089
Miguel Garcia <miguel.garcia@gmail.com>
2011-11-15 00:47:23 -0300

    delta 002

---
+++ meregistro/deltas/002.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+DELETE FROM seguridad_perfil WHERE ambito_id > 25;
+DELETE FROM seguridad_ambito WHERE id > 25;
+
+insert into deltas_sql (numero, app, comentario) values ('002', 'Seguridad', 'Borrado de ambitos que no representan nada en el modelo.');
+
+COMMIT;
+
