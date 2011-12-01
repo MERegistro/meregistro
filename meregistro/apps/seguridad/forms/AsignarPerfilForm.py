0c91d6206d0d066aea7d538a3e090518e295bac0
Miguel Garcia <miguel.garcia@gmail.com>
2011-12-01 01:13:09 -0300

    Ticket #96

--- meregistro/apps/seguridad/forms/AsignarPerfilForm.py
+++ meregistro/apps/seguridad/forms/AsignarPerfilForm.py
@@ -5,5 +5,5 @@ from apps.seguridad.models import Ambito, Rol
 
 
 class AsignarPerfilForm(forms.Form):
-    rol = forms.ModelChoiceField(queryset=Rol.objects, label='rol')
+    rol = forms.ModelChoiceField(queryset=Rol.objects, label='rol', empty_label=None)
     ambito = forms.ModelChoiceField(queryset=Ambito.objects, label='ambito')
