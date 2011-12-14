a9892a63bca478c0e408e5e223f62e996623b87c
Luciano <luciano.baraglia[at]gmail.com>
2011-12-13 21:34:37 -0300

    Pequeño cambio en delta 013

--- meregistro/deltas/013.sql
+++ meregistro/deltas/013.sql
@@ -4,7 +4,7 @@ BEGIN;
 ALTER TABLE registro_establecimiento ALTER COLUMN cue TYPE VARCHAR(9);
 -- Index: registro_establecimiento_cue_idx
 -- DROP INDEX registro_establecimiento_cue_idx;
-CREATE INDEX registro_establecimiento_cue_idx
+CREATE UNIQUE INDEX registro_establecimiento_cue_idx
   ON registro_establecimiento
   USING btree
   (cue);
