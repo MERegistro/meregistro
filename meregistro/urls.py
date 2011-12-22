a1fcf2f68cf6ec9d5e71a50c173132d44264e4f4
Luciano <luciano.baraglia[at]gmail.com>
2011-12-22 01:32:09 -0300

    Commit para mergear

--- meregistro/urls.py
+++ meregistro/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 import apps.seguridad.urls as seguridad_urls
 import apps.registro.urls as registro_urls
 import apps.titulos.urls as titulos_urls
+import apps.reportes.urls as reportes_urls
 
 urlpatterns = patterns('',
     url(r'^login', 'apps.seguridad.views.login', name='login'),
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^seguridad/', include(seguridad_urls)),
     url(r'^registro/', include(registro_urls)),
     url(r'^titulos/', include(titulos_urls)),
+    url(r'^reportes/', include(reportes_urls)),
     url(r'^seleccionarPerfil', 'apps.seguridad.views.seleccionar_perfil', name='seleccionarPerfil'),
     url(r'^' + settings.STATIC_URL_PATH + '/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_DOC_ROOT}),
