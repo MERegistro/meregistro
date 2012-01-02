344eb80309b743c09952a2ee3aeb247bc99fd11f
Luciano <luciano.baraglia[at]gmail.com>
2012-01-02 00:06:28 -0300

    Credencial para ver detalle del establecimiento - Ticket #123

--- meregistro/deltas/018.sql
+++ meregistro/deltas/018.sql
@@ -8,9 +8,14 @@ INSERT INTO seguridad_rol_credenciales (rol_id, credencial_id) VALUES
 (SELECT id FROM seguridad_rol WHERE nombre = 'AdminTitulos'), 
 (SELECT id FROM seguridad_credencial WHERE nombre = 'reg_establecimiento_ver')
 );
+INSERT INTO seguridad_rol_credenciales (rol_id, credencial_id) VALUES
+(
+(SELECT id FROM seguridad_rol WHERE nombre = 'ReferenteJurisdiccional'), 
+(SELECT id FROM seguridad_credencial WHERE nombre = 'reg_establecimiento_ver')
+);
 ---------------------------------------
 
-INSERT INTO deltas_sql (numero, app, comentario) VALUES ('018', 'Seguridad', 'Credencial de ver detalle del establecimiento al rol administrador de títulos nacionales - Ticket #123');
+INSERT INTO deltas_sql (numero, app, comentario) VALUES ('018', 'Seguridad', 'Credencial de ver detalle del establecimiento al rol administrador de títulos nacionales y referente- Ticket #123');
 
 --
 COMMIT;
