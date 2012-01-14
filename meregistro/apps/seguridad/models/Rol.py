77817ca4e15bdfd443d070b104a177ffe892cc86
Miguel Garcia <miguel.garcia@gmail.com>
2012-01-14 00:26:20 -0300

    #134

--- meregistro/apps/seguridad/models/Rol.py
+++ meregistro/apps/seguridad/models/Rol.py
@@ -8,7 +8,7 @@ class Rol(models.Model):
     nombre = models.CharField(max_length=40)
     descripcion = models.CharField(max_length=255)
     credenciales = models.ManyToManyField(Credencial, related_name='roles')
-    roles_asignables = models.ManyToManyField('self', related_name='roles_asignadores')
+    roles_asignables = models.ManyToManyField('self', related_name='roles_asignadores', symmetrical=False)
 
     class Meta:
         app_label = 'seguridad'
