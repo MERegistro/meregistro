5a57b5688f46714b6febbe81334f1c15729889df
Luciano <luciano.baraglia[at]gmail.com>
2012-01-25 00:49:55 -0300

    Modificación en último delta

--- meregistro/deltas/022.sql
+++ meregistro/deltas/022.sql
@@ -13,6 +13,10 @@ INSERT INTO seguridad_rol_credenciales (rol_id, credencial_id) VALUES
 ---------------------------------------
 
 ALTER TABLE registro_establecimiento ALTER COLUMN tipo_normativa_id DROP NOT NULL;
+ALTER TABLE registro_establecimiento_version ALTER COLUMN tipo_normativa_id DROP NOT NULL;
+
+ALTER TABLE registro_establecimiento ADD COLUMN solicitud_filename character varying(100);
+ALTER TABLE registro_establecimiento_version ADD COLUMN solicitud_filename character varying(100);
 
 -- La secuencia está estropeada
 SELECT SETVAL('registro_establecimiento_id_seq', (SELECT MAX(id) FROM registro_establecimiento));
