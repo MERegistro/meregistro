dfbf9b7b8157d12a73e774a54eb2363b17206b58
Luciano <luciano.baraglia[at]gmail.com>
2012-03-20 00:09:32 -0300

    Cambio en delta

--- meregistro/deltas/049.sql
+++ meregistro/deltas/049.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 ALTER TABLE registro_tipo_conexion ALTER COLUMN nombre TYPE VARCHAR(100);
 
-INSERT INTO deltas_sql (numero, app, comentario) VALUES ('049', '', '');
+INSERT INTO deltas_sql (numero, app, comentario) VALUES ('049', '', 'Se cambia el campo nombre de tipo_conexion');
 
 COMMIT;
