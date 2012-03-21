b13f2936a01604376340579537d0f3ab6b3f0f0a
Miguel Garcia <miguel.garcia@gmail.com>
2012-03-21 20:24:01 -0300

    #192

--- meregistro/apps/registro/forms/EstablecimientoAutoridadForm.py
+++ meregistro/apps/registro/forms/EstablecimientoAutoridadForm.py
@@ -9,7 +9,10 @@ from django.core.exceptions import ValidationError
 class EstablecimientoAutoridadForm(ModelForm):
     cargo = forms.ModelChoiceField(queryset=AutoridadCargo.objects.order_by('descripcion'), label='Cargo', required=True, empty_label=None)
     tipo_documento = forms.ModelChoiceField(queryset=TipoDocumento.objects.order_by('descripcion'), label='Tipo de documento', required=False)
-    fecha_nacimiento = forms.DateField(input_formats=['%d/%m/%Y', '%d/%m/%y'], required=False)
+    fecha_nacimiento = forms.DateField(
+        input_formats=['%d/%m/%Y', '%d/%m/%y'],
+        required=False,
+        widget=forms.TextInput(attrs={'class':'datePicker', 'readonly':'true'}))
 
     class Meta:
         model = EstablecimientoAutoridad
