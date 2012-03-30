c6cb3c4dd8a9be52fd71bcac7e31b8bd349a1151
Luciano <luciano.baraglia[at]gmail.com>
2012-03-29 21:17:47 -0300

    Pequeños cambios en info edilicia de extensiones áulicas - Ticket #209

--- meregistro/apps/registro/forms/ExtensionAulicaInformacionEdiliciaForm.py
+++ meregistro/apps/registro/forms/ExtensionAulicaInformacionEdiliciaForm.py
@@ -8,7 +8,7 @@ from django import forms
 
 
 class ExtensionAulicaInformacionEdiliciaForm(forms.ModelForm):
-    niveles = forms.ModelMultipleChoiceField(queryset = Nivel.objects.all().order_by('nombre'), widget = forms.CheckboxSelectMultiple, required = False)
+    niveles = forms.ModelMultipleChoiceField(queryset=Nivel.objects.all().order_by('nombre'), widget=forms.CheckboxSelectMultiple, required=False)
     verificado = forms.BooleanField(required=False)
 
     class Meta:
@@ -19,7 +19,7 @@ class ExtensionAulicaInformacionEdiliciaForm(forms.ModelForm):
         try:
             tipo_dominio = self.cleaned_data['tipo_dominio']
             tipo_compartido = self.cleaned_data['tipo_compartido']
-            if tipo_dominio.id == TipoDominio.objects.get(descripcion = 'Compartido').id:
+            if tipo_dominio.id == TipoDominio.objects.get(descripcion='Compartido').id:
                 if tipo_compartido is None:
                     raise ValidationError('Si el uso del edificio es compartido, debe detallar lo siguiente.')
         except KeyError:
@@ -30,7 +30,7 @@ class ExtensionAulicaInformacionEdiliciaForm(forms.ModelForm):
         niveles = self.cleaned_data['niveles']
         try:
             tipo_compartido = self.cleaned_data['tipo_compartido']
-            if tipo_compartido is not None and tipo_compartido.id == TipoCompartido.objects.get(descripcion = 'ExtensionAulica de otro nivel').id:
+            if tipo_compartido is not None and tipo_compartido.id == TipoCompartido.objects.get(descripcion='Establecimiento de otro nivel').id:
                 if len(niveles) == 0:
                     raise ValidationError('Debe especificar con qué niveles comparte el edificio.')
         except KeyError:
