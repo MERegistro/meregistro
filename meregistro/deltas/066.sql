f23d78a5d4fd44032dfe05ef456f20c3f370d866
Luciano <luciano.baraglia[at]gmail.com>
2012-04-10 23:19:45 -0300

    Modificación delta 066

--- meregistro/deltas/066.sql
+++ meregistro/deltas/066.sql
@@ -4,21 +4,21 @@ BEGIN;
 -- Dependencia Temporal de Gestión Estatal de Ciudad Autónoma de Buenos Aires
 -- El ambito parent era Chubut -> /1/5/34/
 UPDATE seguridad_ambito
-SET "path" = '1/6/34/', parent_id = 6 
+SET "path" = '/1/6/34/', parent_id = 6 
 WHERE id = 34;
 
  
 -- Dependencia Temporal de Gestión Estatal de Corrientes
 -- El ambito parent era Ciudad Autónoma de Buenos Aires -> /1/6/35/
 UPDATE seguridad_ambito
-SET "path" = '1/7/35/', parent_id = 7
+SET "path" = '/1/7/35/', parent_id = 7
 WHERE id = 35;
 
  
 -- Dependencia Temporal de Gestión Estatal de Entre Ríos
 -- El ambito parent era Corrientes -> /1/7/36/
 UPDATE seguridad_ambito
-SET "path" = '1/9/36/', parent_id = 9
+SET "path" = '/1/9/36/', parent_id = 9
 WHERE id = 36;
 
 
