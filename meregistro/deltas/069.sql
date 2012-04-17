6623ea163eceba1786be3f8070c353cb8e655712
Luciano <luciano.baraglia[at]gmail.com>
2012-04-16 23:29:32 -0300

    Cambio de SQL por la versión del motor de DB

--- meregistro/deltas/069.sql
+++ meregistro/deltas/069.sql
@@ -1,7 +1,8 @@
 BEGIN;
 
 -- Modifica todos los ámbitos que quedaron después de la corrección del delta 066
-
+/*
+-- Versiones posteriores
 UPDATE seguridad_ambito
 SET path = CONCAT('/1/6/34/' || SUBSTRING(path FROM 9))
 WHERE path LIKE '/1/5/34/%';
@@ -12,6 +13,19 @@ WHERE path LIKE '/1/6/35/%';
 
 UPDATE seguridad_ambito
 SET path = CONCAT('/1/9/36/' || SUBSTRING(path FROM 9))
+WHERE path LIKE '/1/7/36/%';
+*/
+
+UPDATE seguridad_ambito
+SET path = '/1/6/34/' || SUBSTRING(path FROM 9)
+WHERE path LIKE '/1/5/34/%';
+
+UPDATE seguridad_ambito
+SET path = '/1/7/35/' || SUBSTRING(path FROM 9)
+WHERE path LIKE '/1/6/35/%';
+
+UPDATE seguridad_ambito
+SET path = '/1/9/36/' || SUBSTRING(path FROM 9)
 WHERE path LIKE '/1/7/36/%';
 
 INSERT INTO deltas_sql (numero, app, comentario) VALUES ('069', 'Seguridad', 'Ámbitos erróneos - #218');
