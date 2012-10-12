d2b4967e4266764d1036c27a87b61979a31ea022
Miguel Angel Garcia <miguel.garcia@gmail.com>
2012-10-12 01:57:55 -0300

    #297

---
+++ meregistro/deltas/098.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+DROP TABLE titulos_titulo_jurisd_modalidad_distancia;
+DROP TABLE titulos_titulo_jurisd_modalidad_presencial;
+
+
+INSERT INTO deltas_sql (numero, app, comentario) VALUES ('098', 'Titulos', 'Ticket #297');
+
+COMMIT;
