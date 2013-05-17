843d24a9d2897ee561fe18cf470d621f571695ed
Miguel Angel Garcia <miguel.garcia@gmail.com>
2013-05-17 01:39:11 -0300

    #319

--- meregistro/deltas/104.sql
+++ meregistro/deltas/104.sql
@@ -3,15 +3,15 @@ BEGIN;
 ---------------------------------------
 
 DELETE FROM seguridad_rol_credenciales WHERE credencial_id =
-(SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_nacional_consulta');
+(SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_consulta');
 
 DELETE FROM seguridad_credencial_credenciales_hijas WHERE from_credencial_id =
-(SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_nacional_consulta')
-OR to_credencial_id = (SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_nacional_consulta');
+(SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_consulta')
+OR to_credencial_id = (SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_consulta');
 
 
 DELETE FROM seguridad_credencial
-WHERE nombre = 'tit_titulo_nacional_consulta';
+WHERE nombre = 'tit_titulo_consulta';
 
 ---------------------------------------
 
