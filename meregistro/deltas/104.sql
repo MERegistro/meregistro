1b21d251fcc69639dbcfd8ec023c05cd0b350d6f
Miguel Angel Garcia <miguel.garcia@gmail.com>
2013-05-17 01:36:08 -0300

    #319

---
+++ meregistro/deltas/104.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+---------------------------------------
+
+DELETE FROM seguridad_rol_credenciales WHERE credencial_id =
+(SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_nacional_consulta');
+
+DELETE FROM seguridad_credencial_credenciales_hijas WHERE from_credencial_id =
+(SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_nacional_consulta')
+OR to_credencial_id = (SELECT id FROM seguridad_credencial WHERE nombre = 'tit_titulo_nacional_consulta');
+
+
+DELETE FROM seguridad_credencial
+WHERE nombre = 'tit_titulo_nacional_consulta';
+
+---------------------------------------
+
+INSERT INTO deltas_sql (numero, app, comentario) VALUES ('104', 'Seguridad', 'Ticket #319');
+
+COMMIT;
