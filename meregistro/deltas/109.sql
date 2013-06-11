76038c3fbf543a8e24b4157fd04254c4a3dfb4e7
Miguel Angel Garcia <miguel.garcia@gmail.com>
2013-06-11 20:31:31 -0300

    fix estados de cohortes

---
+++ meregistro/deltas/109.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+UPDATE titulos_estado_cohorte_anexo SET nombre = 'Registrada' WHERE nombre = 'Aceptada';
+UPDATE titulos_estado_cohorte_extension_aulica SET nombre = 'Registrada' WHERE nombre = 'Aceptada';
+UPDATE titulos_estado_cohorte_establecimiento SET nombre = 'Registrada' WHERE nombre = 'Aceptada';
+
+UPDATE titulos_estado_cohorte_anexo SET nombre = 'Aceptada' WHERE nombre = 'Asignada';
+UPDATE titulos_estado_cohorte_extension_aulica SET nombre = 'Aceptada' WHERE nombre = 'Asignada';
+UPDATE titulos_estado_cohorte_establecimiento SET nombre = 'Aceptada' WHERE nombre = 'Asignada';
+
+INSERT INTO deltas_sql (numero, app, comentario) VALUES ('109', 'Titulos', 'Ticket #325');
+
+COMMIT;
