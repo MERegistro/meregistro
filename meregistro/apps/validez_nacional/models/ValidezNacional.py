565e574273c14e2852203a9cdbc8e3a32533a831
Miguel Angel Garcia <miguel.garcia@gmail.com>
2013-06-13 01:35:59 -0300

    fix typo

--- meregistro/apps/validez_nacional/models/ValidezNacional.py
+++ meregistro/apps/validez_nacional/models/ValidezNacional.py
@@ -21,33 +21,3 @@ class ValidezNacional(models.Model):
 		app_label = 'validez_nacional'
 		db_table = 'validez_nacional_validez_nacional'
 
-	def __unicode__(self):
-		return self.titulo.nombre
-
-	"Sobreescribo el init para agregarle propiedades"
-	def __init__(self, *args, **kwargs):
-		super(Solicitud, self).__init__(*args, **kwargs)
-		self.estados = self.get_estados()
-
-	"Sobreescribo para eliminar los objetos relacionados"
-	def delete(self, *args, **kwargs):
-		for normativa in self.normativas.all():
-			normativa.delete()
-		for est in self.estados.all():
-			est.delete()
-		super(Solicitud, self).delete(*args, **kwargs)
-
-	def registrar_estado(self):
-		from apps.validez_nacional.models import SolicitudEstado
-		registro = SolicitudEstado(estado=self.estado)
-		registro.fecha = datetime.date.today()
-		registro.solicitud_id = self.id
-		registro.save()
-
-	def get_estados(self):
-		from apps.titulos.models import SolicitudEstado
-		try:
-			estados = SolicitudEstado.objects.filter(solicitud=self).order_by('fecha', 'id')
-		except:
-			estados = {}
-		return estados
