1ba604c9660f2d7560966de2b47a188e7ead8f5e
Luciano Baraglia <luciano.baraglia@gmail.com>
2013-08-22 10:36:35 -0300

    Modificaciones a Oferta 2013 en la Home - #341

--- meregistro/apps/registro/models/AnexoDomicilio.py
+++ meregistro/apps/registro/models/AnexoDomicilio.py
@@ -8,24 +8,28 @@ from apps.seguridad.audit import audit
 
 @audit
 class AnexoDomicilio(models.Model):
-    anexo = models.ForeignKey(Anexo, related_name = 'anexo_domicilio')
-    tipo_domicilio = models.ForeignKey(TipoDomicilio)
-    localidad = models.ForeignKey(Localidad, related_name = 'anexo_localidad')
-    calle = models.CharField(max_length=100)
-    altura = models.CharField(max_length=15)
-    referencia = models.CharField(max_length=255, null=True, blank=True)
-    cp = models.CharField(max_length=20)
+	
+	TIPO_POSTAL = 'Postal'
+	TIPO_INSTITUCIONAL = 'Institucional'
+	
+	anexo = models.ForeignKey(Anexo, related_name = 'anexo_domicilio')
+	tipo_domicilio = models.ForeignKey(TipoDomicilio)
+	localidad = models.ForeignKey(Localidad, related_name = 'anexo_localidad')
+	calle = models.CharField(max_length=100)
+	altura = models.CharField(max_length=15)
+	referencia = models.CharField(max_length=255, null=True, blank=True)
+	cp = models.CharField(max_length=20)
 
-    class Meta:
-        app_label = 'registro'
-        db_table = 'registro_anexo_domicilio'
+	class Meta:
+		app_label = 'registro'
+		db_table = 'registro_anexo_domicilio'
 
-    def __unicode__(self):
-        if self.cp:
-            cp = u" (CP: " + self.cp + ")"
-        else:
-            cp = u""
-        return u"%s %s - %s %s" % (self.calle, self.altura, self.localidad.nombre, cp)
+	def __unicode__(self):
+		if self.cp:
+			cp = u" (CP: " + self.cp + ")"
+		else:
+			cp = u""
+		return u"%s %s - %s %s" % (self.calle, self.altura, self.localidad.nombre, cp)
 
-    def __init__(self, *args, **kwargs):
-        super(AnexoDomicilio, self).__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super(AnexoDomicilio, self).__init__(*args, **kwargs)
