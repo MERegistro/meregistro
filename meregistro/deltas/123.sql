d258f60827dbc6cef3388749cc66f040b7f66044
Luciano Baraglia <luciano.baraglia@gmail.com>
2014-01-26 01:39:46 -0300

    Fix delta 123

--- meregistro/deltas/123.sql
+++ meregistro/deltas/123.sql
@@ -2,24 +2,24 @@ BEGIN;
 
 -- Agregar cues a ambitos de establecimientos
 UPDATE seguridad_ambito a
-SET descripcion = CONCAT(est.cue || ' - ' || descripcion)
+SET descripcion = est.cue || ' - ' || descripcion
 FROM registro_establecimiento est 
 WHERE a.id = est.ambito_id 
-AND a.tipo_id = 4
+AND a.tipo_id = 4;
 
 -- Agregar cues a ambitos de anexos
 UPDATE seguridad_ambito a
-SET descripcion = CONCAT(anexo.cue || ' - ' || descripcion)
+SET descripcion = anexo.cue || ' - ' || descripcion
 FROM registro_anexo anexo 
 WHERE a.id = anexo.ambito_id 
-AND a.tipo_id = 5
+AND a.tipo_id = 5;
 
 -- Agregar cues a ambitos de extensiones áulicas
 UPDATE seguridad_ambito a
-SET descripcion = CONCAT(ea.cue || ' - ' || descripcion)
+SET descripcion = ea.cue || ' - ' || descripcion
 FROM registro_extension_aulica ea 
 WHERE a.id = ea.ambito_id 
-AND a.tipo_id = 6
+AND a.tipo_id = 6;
 
 INSERT INTO deltas_sql (numero, app, comentario) VALUES ('123', 'Registro', 'CUE en descripción de ámbito de UE - Ticket #376');
 
