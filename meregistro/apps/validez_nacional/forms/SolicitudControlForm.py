0cb109c2037ba8153e5fe7f44dc12bc2a327277a
Luciano Baraglia <luciano.baraglia@gmail.com>
2014-04-02 02:36:55 -0300

    Validación de dígitos del año en número de expediente - #389

--- meregistro/apps/validez_nacional/forms/SolicitudControlForm.py
+++ meregistro/apps/validez_nacional/forms/SolicitudControlForm.py
@@ -3,10 +3,16 @@ from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from django import forms
 from apps.validez_nacional.models import Solicitud, EstadoSolicitud
-
+from django.core.validators import RegexValidator
 
 class SolicitudControlForm(forms.ModelForm):
-	nro_expediente = forms.CharField(max_length=200, label='Nro. Expediente', required=False)
+	nro_expediente = forms.CharField(max_length=200, label='Nro. Expediente', required=False, validators=[
+		RegexValidator(
+			'^.*\/[0-9]{2}$', 
+			message='El Nro de Expediente debe incluir dos dígitos indicando el año (ej: abcde/14)', 
+			code='expediente_invalido'
+		),
+	])
 	dictamen_cofev = forms.CharField(max_length=200, label='Dictamen Cofev', required=False)
 	normativas_nacionales = forms.CharField(max_length=99, label='Normativas Nacionales', required=False)
 	estado = forms.ModelChoiceField(queryset=EstadoSolicitud.objects.order_by('nombre'), label='Estado', required=True, empty_label=None)
