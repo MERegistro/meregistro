f31c1f7bc41bc2cb950e77bcd5d9ef02b66e994f
Luciano Baraglia <luciano.baraglia@gmail.com>
2014-04-14 14:49:55 -0300

    Avances #387

---
+++ meregistro/deltas/129.sql
@@ -0,0 +1,35 @@
+BEGIN;
+
+--
+
+INSERT INTO seguridad_rol (nombre, descripcion, padre_id) VALUES
+('SoloConsultaNacional', 'Sólo Consulta Ámbito Nacional', (SELECT id FROM seguridad_rol WHERE nombre = 'AdminNacional')),
+('SoloConsultaJurisdiccional', 'Sólo Consulta Ámbito Jurisdiccional', (SELECT id FROM seguridad_rol WHERE nombre = 'AdminNacional'));
+
+-- Paths
+UPDATE seguridad_rol
+SET "path" = '/1/2/' || id || '/'
+WHERE nombre IN('SoloConsultaNacional', 'SoloConsultaJurisdiccional')
+;
+-- Roles asignables
+INSERT INTO seguridad_rol_roles_asignables (from_rol_id, to_rol_id)
+VALUES (
+  (SELECT id FROM seguridad_rol WHERE nombre = 'AdminNacional'),
+  (SELECT id FROM seguridad_rol WHERE nombre = 'SoloConsultaNacional')
+);
+INSERT INTO seguridad_rol_roles_asignables (from_rol_id, to_rol_id)
+VALUES (
+  (SELECT id FROM seguridad_rol WHERE nombre = 'AdminNacional'),
+  (SELECT id FROM seguridad_rol WHERE nombre = 'SoloConsultaJurisdiccional')
+);
+-- Tipos de ámbito asignables
+INSERT INTO seguridad_rol_tipos_ambito_asignable (rol_id, tipoambito_id)
+VALUES ((SELECT id FROM seguridad_rol WHERE nombre = 'SoloConsultaNacional'), 1);
+INSERT INTO seguridad_rol_tipos_ambito_asignable (rol_id, tipoambito_id)
+VALUES ((SELECT id FROM seguridad_rol WHERE nombre = 'SoloConsultaJurisdiccional'), 2);
+
+-------------------------------------
+
+INSERT INTO deltas_sql (numero, app, comentario) VALUES ('129', 'Seguridad', 'Roles de sólo consulta - #387');
+
+COMMIT;
