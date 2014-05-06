bccac76793b3c71516ac4a2f4a02cb4d2eacc37a
Luciano Baraglia <luciano.baraglia@gmail.com>
2014-05-06 20:31:31 -0300

    Arreglo reporte de cohortes jurisdiccionales - #402

--- meregistro/apps/reportes/views/cohorte_jurisdiccional.py
+++ meregistro/apps/reportes/views/cohorte_jurisdiccional.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime, date
 from apps.seguridad.decorators import login_required, credential_required
 from apps.seguridad.models import Usuario, Perfil
-from apps.titulos.models import Carrera, CarreraJurisdiccional
+from apps.titulos.models import Carrera, CarreraJurisdiccional, CarreraJurisdiccionalCohorte
 import csv
 from apps.reportes.models import Reporte
 
@@ -13,14 +13,13 @@ from apps.reportes.models import Reporte
 #@credential_required('tit_cohorte_jurisdiccional_consulta')
 def cohortes_jurisdiccionales(request, q):
 	filename = 'cohortes_jurisdiccionales_' + str(date.today()) + '.xls'
-	reporte = Reporte(headers=['CARRERA', 'COHORTES SOLICITADAS', 'COHORTES AUTORIZADAS', 'COHORTES GENERADAS', 'ESTADOS'], filename=filename)
+	reporte = Reporte(headers=['CARRERA', 'JURISDICCIÓN', 'COHORTES GENERADAS', 'ESTADO'], filename=filename)
 	for cj in q:
-		cohortes_solicitadas = str(cj.datos_cohorte.get().primera_cohorte_solicitada) + ' - ' + str(cj.datos_cohorte.get().ultima_cohorte_solicitada)
 		cohortes_generadas = ' - '.join("%s" % c.anio for c in cj.cohortes.all().order_by('anio'))
 		if cj.estado is None:
 			estado_nombre = ''
 		else:
 			estado_nombre = cj.estado.nombre.encode('utf8')
-		reporte.rows.append([cj.carrera.nombre.encode('utf8'), cohortes_solicitadas, '', cohortes_generadas, estado_nombre])
+		reporte.rows.append([cj.carrera.nombre.encode('utf8'), cj.jurisdiccion.nombre, cohortes_generadas, estado_nombre])
 
 	return reporte.as_csv()
