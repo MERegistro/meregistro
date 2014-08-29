b23d91f2f8f1e9885fe9bc2969165c086ed3320c
Luciano Baraglia <luciano.baraglia@gmail.com>
2014-08-29 03:04:15 -0300

    Diseño V2 para la portada - #401

--- meregistro/apps/registro/views/index.py
+++ meregistro/apps/registro/views/index.py
@@ -6,8 +6,10 @@ from apps.seguridad.decorators import login_required
 def index(request):
     if request.user.is_authenticated():
         extend_template = 'base_registro.html'
+        template = 'registro/index/index.html'
     else:
         extend_template = 'base.html'
-    return my_render(request, 'registro/index/index.html', {
+        template = 'registro/index/index_no_autenticado.html'
+    return my_render(request, template, {
         'extend_template': extend_template
     })
