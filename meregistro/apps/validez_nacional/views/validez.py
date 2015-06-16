9e59a403b58531c4ee9da36786211cf3e5063191
Luciano Baraglia <luciano.baraglia@gmail.com>
2015-06-16 16:00:10 -0300

    Duplicación de registro de Validez - #471

--- meregistro/apps/validez_nacional/views/validez.py
+++ meregistro/apps/validez_nacional/views/validez.py
@@ -19,97 +19,122 @@ ITEMS_PER_PAGE = 50
 @credential_required('validez_nacional_validez_index')
 def index(request):
 
-	jur = request.get_perfil().jurisdiccion()
+    jur = request.get_perfil().jurisdiccion()
 
-	if request.method == 'GET':
-		form_filter = ValidezNacionalFormFilters(request.GET, jurisdiccion=jur)
-	else:
-		form_filter = ValidezNacionalFormFilters(jurisdiccion=jur)
-	q = build_query(form_filter, 1, request)
+    if request.method == 'GET':
+        form_filter = ValidezNacionalFormFilters(request.GET, jurisdiccion=jur)
+    else:
+        form_filter = ValidezNacionalFormFilters(jurisdiccion=jur)
+    q = build_query(form_filter, 1, request)
 
-	"""
-	try:
-		if request.GET['export'] == '1':
-			return reporte_solicitudes(request, q)
-	except KeyError:
-		pass
-	"""
-	
-	paginator = Paginator(q, ITEMS_PER_PAGE)
+    """
+    try:
+        if request.GET['export'] == '1':
+            return reporte_solicitudes(request, q)
+    except KeyError:
+        pass
+    """
+    
+    paginator = Paginator(q, ITEMS_PER_PAGE)
 
-	try:
-		page_number = int(request.GET['page'])
-	except (KeyError, ValueError):
-		page_number = 1
-	# chequear los límites
-	if page_number < 1:
-		page_number = 1
-	elif page_number > paginator.num_pages:
-		page_number = paginator.num_pages
+    try:
+        page_number = int(request.GET['page'])
+    except (KeyError, ValueError):
+        page_number = 1
+    # chequear los límites
+    if page_number < 1:
+        page_number = 1
+    elif page_number > paginator.num_pages:
+        page_number = paginator.num_pages
 
-	page = paginator.page(page_number)
-	objects = page.object_list
-	return my_render(request, 'validez_nacional/validez/index.html', {
-		'form_filters': form_filter,
-		'objects': objects,
-		'paginator': paginator,
-		'page': page,
-		'page_number': page_number,
-		'pages_range': range(1, paginator.num_pages + 1),
-		'next_page': page_number + 1,
-		'prev_page': page_number - 1,
-		#'export_url': Reporte.build_export_url(request.build_absolute_uri()),
-	})
+    page = paginator.page(page_number)
+    objects = page.object_list
+    return my_render(request, 'validez_nacional/validez/index.html', {
+        'form_filters': form_filter,
+        'objects': objects,
+        'paginator': paginator,
+        'page': page,
+        'page_number': page_number,
+        'pages_range': range(1, paginator.num_pages + 1),
+        'next_page': page_number + 1,
+        'prev_page': page_number - 1,
+        #'export_url': Reporte.build_export_url(request.build_absolute_uri()),
+    })
 
 
 def build_query(filters, page, request):
-	"""
-	Construye el query de búsqueda a partir de los filtros.
-	"""
-	return filters.buildQuery().order_by('titulo_nacional', 'primera_cohorte')
+    """
+    Construye el query de búsqueda a partir de los filtros.
+    """
+    return filters.buildQuery().order_by('titulo_nacional', 'primera_cohorte')
 
 
 @login_required
 @credential_required('validez_nacional_validez_editar')
 def edit(request, validez_id):
-	validez = ValidezNacional.objects.get(pk=validez_id)
-	
-	if request.method == 'POST':
-		form = ValidezNacionalForm(request.POST, instance=validez)
-		if form.is_valid():
-			v = form.save()
+    validez = ValidezNacional.objects.get(pk=validez_id)
+    
+    if request.method == 'POST':
+        form = ValidezNacionalForm(request.POST, instance=validez)
+        if form.is_valid():
+            v = form.save()
 
-			request.set_flash('success', 'Datos actualizados correctamente.')
-		else:
-			request.set_flash('warning', 'Ocurrió un error actualizando los datos.')
-	else:
-		form = ValidezNacionalForm(instance=validez)
-	
-	return my_render(request, 'validez_nacional/validez/edit.html', {
-		'validez': validez,
-		'form': form,
-	})
+            request.set_flash('success', 'Datos actualizados correctamente.')
+        else:
+            request.set_flash('warning', 'Ocurrió un error actualizando los datos.')
+    else:
+        form = ValidezNacionalForm(instance=validez)
+    
+    return my_render(request, 'validez_nacional/validez/edit.html', {
+        'validez': validez,
+        'form': form,
+    })
 
 
 @login_required
 @credential_required('validez_nacional_validez_editar')
 def eliminar(request, validez_nacional_id):
-	"""
-	No sólo deberá eliminarse el registro de la tabla validez_nacional_validez_nacional, 
-	sino que también deberá eliminarse la asignación que había hacia ese CUE de la 
-	Solicitud de Validez Nacional correspondiente (en la tabla validez_nacional_solicitud_establecimientos
-	o en la tabla validez_nacional_solicitud_anexos según sea un registro de una Sede o un Anexo), 
-	como si el registro que se está eliminando nunca hubiera sido parte de la solicitud original.
-	"""
-	validez_nacional = ValidezNacional.objects.get(pk=validez_nacional_id)
-	
-	if request.method == 'POST':
-		if int(request.POST['validez_nacional_id']) != int(validez_nacional_id):
-			raise Exception('Error en la consulta!')
-		validez_nacional.delete()
-		request.set_flash('success', 'El registro de Validez Nacional fue eliminado correctamente.')
-		""" Redirecciono para evitar el reenvío del form """
-		return HttpResponseRedirect(reverse('validezNacionalIndex'))
-	return my_render(request, 'validez_nacional/validez/eliminar.html', {
-		'validez_nacional_id': validez_nacional.id,
-	})
+    """
+    No sólo deberá eliminarse el registro de la tabla validez_nacional_validez_nacional, 
+    sino que también deberá eliminarse la asignación que había hacia ese CUE de la 
+    Solicitud de Validez Nacional correspondiente (en la tabla validez_nacional_solicitud_establecimientos
+    o en la tabla validez_nacional_solicitud_anexos según sea un registro de una Sede o un Anexo), 
+    como si el registro que se está eliminando nunca hubiera sido parte de la solicitud original.
+    """
+    validez_nacional = ValidezNacional.objects.get(pk=validez_nacional_id)
+    
+    if request.method == 'POST':
+        if int(request.POST['validez_nacional_id']) != int(validez_nacional_id):
+            raise Exception('Error en la consulta!')
+        validez_nacional.delete()
+        request.set_flash('success', 'El registro de Validez Nacional fue eliminado correctamente.')
+        """ Redirecciono para evitar el reenvío del form """
+        return HttpResponseRedirect(reverse('validezNacionalIndex'))
+    return my_render(request, 'validez_nacional/validez/eliminar.html', {
+        'validez_nacional_id': validez_nacional.id,
+    })
+
+    
+
+@login_required
+@credential_required('validez_nacional_validez_editar')
+def duplicar(request, validez_id):
+    validez = ValidezNacional.objects.get(pk=validez_id)
+    
+    if request.method == 'POST':
+        validez.pk = None
+        form = ValidezNacionalForm(request.POST, instance=validez)
+        if form.is_valid():
+            validez = form.save()
+            return HttpResponseRedirect(reverse('validezNacionalEditarValidez', args=[validez.id]))
+
+            request.set_flash('success', 'Registro de Validez duplicado correctamente.')
+        else:
+            request.set_flash('warning', 'Ocurrió un error duplicando el registro.')
+    else:
+        form = ValidezNacionalForm(instance=validez)
+    
+    return my_render(request, 'validez_nacional/validez/edit.html', {
+        'validez': validez,
+        'form': form,
+    })
