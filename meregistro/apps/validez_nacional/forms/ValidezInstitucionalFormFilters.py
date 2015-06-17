57b6622f40a28e3ef9646f8de633c9bc14fb1cf5
Luciano Baraglia <luciano.baraglia@gmail.com>
2015-06-17 14:10:20 -0300

    Filtro de normativa nacional en Validez Nacional - #469

--- meregistro/apps/validez_nacional/forms/ValidezInstitucionalFormFilters.py
+++ meregistro/apps/validez_nacional/forms/ValidezInstitucionalFormFilters.py
@@ -7,39 +7,39 @@ from apps.titulos.models import Cohorte
 
 ANIOS_COHORTE_CHOICES = [('', '-------')] + [(i, i) for i in range(Cohorte.PRIMER_ANIO, Cohorte.ULTIMO_ANIO)]
 class ValidezInstitucionalFormFilters(forms.Form):
-	cue = forms.CharField(max_length=11, label='CUE', required=False)
-	carrera = forms.CharField(max_length=40, label='Carrera', required=False)
-	titulo_nacional = forms.CharField(max_length=40, label='Título', required=False)
-	primera_cohorte = forms.ChoiceField(label='Primera Cohorte', choices=ANIOS_COHORTE_CHOICES, required=False)
-	normativas_nacionales = forms.CharField(max_length=40, label='Normativa Nacional', required=False)
-	nro_infd = forms.CharField(max_length=40, label='Número INFD', required=False)
+    cue = forms.CharField(max_length=11, label='CUE', required=False)
+    carrera = forms.CharField(max_length=40, label='Carrera', required=False)
+    titulo_nacional = forms.CharField(max_length=40, label='Título', required=False)
+    primera_cohorte = forms.ChoiceField(label='Primera Cohorte', choices=ANIOS_COHORTE_CHOICES, required=False)
+    normativas_nacionales = forms.CharField(max_length=40, label='Normativa Nacional', required=False)
+    nro_infd = forms.CharField(max_length=40, label='Número INFD', required=False)
 
-	def __init__(self, *args, **kwargs):
-		self.establecimiento = kwargs.pop('establecimiento')
-		super(ValidezInstitucionalFormFilters, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.establecimiento = kwargs.pop('establecimiento')
+        super(ValidezInstitucionalFormFilters, self).__init__(*args, **kwargs)
         
         
-	def buildQuery(self, q=None):
-		from itertools import chain
-		"""
-		Crea o refina un query de búsqueda.
-		"""
-		if q is None:
-			q = ValidezNacional.objects.filter(tipo_unidad_educativa=ValidezNacional.TIPO_UE_SEDE, unidad_educativa_id=self.establecimiento.id) \
-			| ValidezNacional.objects.filter(tipo_unidad_educativa=ValidezNacional.TIPO_UE_ANEXO,unidad_educativa_id__in=[a.id for a in self.establecimiento.anexos.all()])
-		if self.is_valid():
-			def filter_by(field):
-				return self.cleaned_data.has_key(field) and self.cleaned_data[field] != '' and self.cleaned_data[field] is not None
-		if filter_by('cue'):
-			q = q.filter(cue__icontains=self.cleaned_data['cue'])
-		if filter_by('carrera'):
-			q = q.filter(carrera__icontains=self.cleaned_data['carrera'])
-		if filter_by('titulo_nacional'):
-			q = q.filter(titulo_nacional__icontains=self.cleaned_data['titulo_nacional'])
-		if filter_by('primera_cohorte'):
-			q = q.filter(primera_cohorte=self.cleaned_data['primera_cohorte'])
-		if filter_by('normativas_nacionales'):
-			q = q.filter(normativas_nacionales__icontains=self.cleaned_data['normativas_nacionales'])
-		if filter_by('nro_infd'):
-			q = q.filter(nro_infd__icontains=self.cleaned_data['nro_infd'])
-		return q
+    def buildQuery(self, q=None):
+        from itertools import chain
+        """
+        Crea o refina un query de búsqueda.
+        """
+        if q is None:
+            q = ValidezNacional.objects.filter(tipo_unidad_educativa=ValidezNacional.TIPO_UE_SEDE, unidad_educativa_id=self.establecimiento.id) \
+            | ValidezNacional.objects.filter(tipo_unidad_educativa=ValidezNacional.TIPO_UE_ANEXO,unidad_educativa_id__in=[a.id for a in self.establecimiento.anexos.all()])
+        if self.is_valid():
+            def filter_by(field):
+                return self.cleaned_data.has_key(field) and self.cleaned_data[field] != '' and self.cleaned_data[field] is not None
+        if filter_by('cue'):
+            q = q.filter(cue__icontains=self.cleaned_data['cue'])
+        if filter_by('carrera'):
+            q = q.filter(carrera__icontains=self.cleaned_data['carrera'])
+        if filter_by('titulo_nacional'):
+            q = q.filter(titulo_nacional__icontains=self.cleaned_data['titulo_nacional'])
+        if filter_by('primera_cohorte'):
+            q = q.filter(primera_cohorte=self.cleaned_data['primera_cohorte'])
+        if filter_by('normativas_nacionales'):
+            q = q.filter(normativas_nacionales__icontains=self.cleaned_data['normativas_nacionales'])
+        if filter_by('nro_infd'):
+            q = q.filter(nro_infd__icontains=self.cleaned_data['nro_infd'])
+        return q
--- meregistro/apps/validez_nacional/forms/ValidezNacionalFormFilters.py
+++ meregistro/apps/validez_nacional/forms/ValidezNacionalFormFilters.py
@@ -7,52 +7,55 @@ from apps.titulos.models import Cohorte
 
 ANIOS_COHORTE_CHOICES = [('', '-------')] + [(i, i) for i in range(Cohorte.PRIMER_ANIO, Cohorte.ULTIMO_ANIO)]
 class ValidezNacionalFormFilters(forms.Form):
-	jurisdiccion = forms.ModelChoiceField(queryset=Jurisdiccion.objects.all().order_by('nombre'), required=False)
-	cue = forms.CharField(max_length=11, label='CUE', required=False)
-	carrera = forms.CharField(max_length=40, label='Carrera', required=False)
-	titulo_nacional = forms.CharField(max_length=40, label='Título', required=False)
-	primera_cohorte = forms.ChoiceField(label='Primera Cohorte Autorizada', choices=ANIOS_COHORTE_CHOICES, required=False)
-	nro_infd = forms.CharField(max_length=40, label='Número INFD', required=False)
+    jurisdiccion = forms.ModelChoiceField(queryset=Jurisdiccion.objects.all().order_by('nombre'), required=False)
+    cue = forms.CharField(max_length=11, label='CUE', required=False)
+    carrera = forms.CharField(max_length=40, label='Carrera', required=False)
+    titulo_nacional = forms.CharField(max_length=40, label='Título', required=False)
+    primera_cohorte = forms.ChoiceField(label='Primera Cohorte Autorizada', choices=ANIOS_COHORTE_CHOICES, required=False)
+    nro_infd = forms.CharField(max_length=40, label='Número INFD', required=False)
+    normativas_nacionales = forms.CharField(max_length=40, label='Normativa Nacional', required=False)
 
 
-	def __init__(self, *args, **kwargs):
-		self.jur = kwargs.pop('jurisdiccion')
-		super(ValidezNacionalFormFilters, self).__init__(*args, **kwargs)
-		if self.jur:
-			choices = [(j.id, j.nombre) for j in Jurisdiccion.objects.filter(pk=self.jur.id)]
-			self.fields['jurisdiccion'] = forms.ChoiceField(choices=choices, required=False)
-		
-	def buildQuery(self, q=None):
-		"""
-		Crea o refina un query de búsqueda.
-		"""
-		if q is None:			 
-			q = ValidezNacional.objects.all()
-		if self.is_valid():
-			def filter_by(field):
-				return self.cleaned_data.has_key(field) and self.cleaned_data[field] != '' and self.cleaned_data[field] is not None
-			if self.jur:
-				from django.db.models import Q
-				q = q.filter(
-					(Q(tipo_unidad_educativa='Sede') & Q(unidad_educativa_id__in=[e.pk for e in Establecimiento.objects.filter(dependencia_funcional__jurisdiccion__id=self.jur.id)])) |
-					(Q(tipo_unidad_educativa='Anexo') & Q(unidad_educativa_id__in=[a.pk for a in Anexo.objects.filter(establecimiento__dependencia_funcional__jurisdiccion__id=self.jur.id)]))
-				)
-			else:
-				if filter_by('jurisdiccion'):
-					# Puede ser sede, anexo o jurisdicción, determinarlo
-					from django.db.models import Q
-					q = q.filter(
-						(Q(tipo_unidad_educativa='Sede') & Q(unidad_educativa_id__in=[e.pk for e in Establecimiento.objects.filter(dependencia_funcional__jurisdiccion__id=self.cleaned_data['jurisdiccion'].id)])) |
-						(Q(tipo_unidad_educativa='Anexo') & Q(unidad_educativa_id__in=[a.pk for a in Anexo.objects.filter(establecimiento__dependencia_funcional__jurisdiccion__id=self.cleaned_data['jurisdiccion'].id)]))
-					)
-			if filter_by('cue'):
-				q = q.filter(cue__icontains=self.cleaned_data['cue'])
-			if filter_by('carrera'):
-				q = q.filter(carrera__icontains=self.cleaned_data['carrera'])
-			if filter_by('titulo_nacional'):
-				q = q.filter(titulo_nacional__icontains=self.cleaned_data['titulo_nacional'])
-			if filter_by('primera_cohorte'):
-				q = q.filter(primera_cohorte=self.cleaned_data['primera_cohorte'])
-			if filter_by('nro_infd'):
-				q = q.filter(nro_infd__icontains=self.cleaned_data['nro_infd'])
-		return q
+    def __init__(self, *args, **kwargs):
+        self.jur = kwargs.pop('jurisdiccion')
+        super(ValidezNacionalFormFilters, self).__init__(*args, **kwargs)
+        if self.jur:
+            choices = [(j.id, j.nombre) for j in Jurisdiccion.objects.filter(pk=self.jur.id)]
+            self.fields['jurisdiccion'] = forms.ChoiceField(choices=choices, required=False)
+        
+    def buildQuery(self, q=None):
+        """
+        Crea o refina un query de búsqueda.
+        """
+        if q is None:             
+            q = ValidezNacional.objects.all()
+        if self.is_valid():
+            def filter_by(field):
+                return self.cleaned_data.has_key(field) and self.cleaned_data[field] != '' and self.cleaned_data[field] is not None
+            if self.jur:
+                from django.db.models import Q
+                q = q.filter(
+                    (Q(tipo_unidad_educativa='Sede') & Q(unidad_educativa_id__in=[e.pk for e in Establecimiento.objects.filter(dependencia_funcional__jurisdiccion__id=self.jur.id)])) |
+                    (Q(tipo_unidad_educativa='Anexo') & Q(unidad_educativa_id__in=[a.pk for a in Anexo.objects.filter(establecimiento__dependencia_funcional__jurisdiccion__id=self.jur.id)]))
+                )
+            else:
+                if filter_by('jurisdiccion'):
+                    # Puede ser sede, anexo o jurisdicción, determinarlo
+                    from django.db.models import Q
+                    q = q.filter(
+                        (Q(tipo_unidad_educativa='Sede') & Q(unidad_educativa_id__in=[e.pk for e in Establecimiento.objects.filter(dependencia_funcional__jurisdiccion__id=self.cleaned_data['jurisdiccion'].id)])) |
+                        (Q(tipo_unidad_educativa='Anexo') & Q(unidad_educativa_id__in=[a.pk for a in Anexo.objects.filter(establecimiento__dependencia_funcional__jurisdiccion__id=self.cleaned_data['jurisdiccion'].id)]))
+                    )
+            if filter_by('cue'):
+                q = q.filter(cue__icontains=self.cleaned_data['cue'])
+            if filter_by('carrera'):
+                q = q.filter(carrera__icontains=self.cleaned_data['carrera'])
+            if filter_by('titulo_nacional'):
+                q = q.filter(titulo_nacional__icontains=self.cleaned_data['titulo_nacional'])
+            if filter_by('primera_cohorte'):
+                q = q.filter(primera_cohorte=self.cleaned_data['primera_cohorte'])
+            if filter_by('nro_infd'):
+                q = q.filter(nro_infd__icontains=self.cleaned_data['nro_infd'])
+            if filter_by('normativas_nacionales'):
+                q = q.filter(normativas_nacionales__icontains=self.cleaned_data['normativas_nacionales'])
+        return q
