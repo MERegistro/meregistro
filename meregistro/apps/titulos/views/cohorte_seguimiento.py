cf1ad724089884171c5ed085dad86808387fb770
Luciano Baraglia <luciano.baraglia@gmail.com>
2016-05-06 13:46:39 -0300

    Pantalla de confirmación de finalización de seguimiento - #494

--- meregistro/apps/titulos/views/cohorte_seguimiento.py
+++ meregistro/apps/titulos/views/cohorte_seguimiento.py
@@ -208,7 +208,7 @@ def rechazar(request, cohorte_ue_id, tipo_unidad_educativa):
 
 @login_required
 #@credential_required('tit_cohorte_aceptar_asignacion')
-def finalizar_seguimiento(request, cohorte_ue_id, tipo_unidad_educativa):
+def confirmar_finalizacion(request, cohorte_ue_id, tipo_unidad_educativa):
     """
     Rechazar cohorte
     """
@@ -237,6 +237,45 @@ def finalizar_seguimiento(request, cohorte_ue_id, tipo_unidad_educativa):
         request.set_flash('warning', 'La cohorte ya se encuentra finalizada.')
 
     return HttpResponseRedirect(reverse(return_url, args=[unidad_educativa.id]))
+
+@login_required
+#@credential_required('tit_cohorte_aceptar_asignacion')
+def finalizar_seguimiento(request, cohorte_ue_id, tipo_unidad_educativa):
+    """
+    Rechazar cohorte
+    """
+    if tipo_unidad_educativa == 'establecimiento':
+        cohorte_unidad_educativa = CohorteEstablecimiento.objects.get(pk=cohorte_ue_id, establecimiento__ambito__path__istartswith=request.get_perfil().ambito.path)
+        unidad_educativa = cohorte_unidad_educativa.establecimiento
+        estado_model = EstadoCohorteEstablecimiento
+        return_url = 'cohortesEstablecimientoIndex'
+    elif tipo_unidad_educativa == 'anexo':
+        cohorte_unidad_educativa = CohorteAnexo.objects.get(pk=cohorte_ue_id, anexo__ambito__path__istartswith=request.get_perfil().ambito.path)
+        unidad_educativa = cohorte_unidad_educativa.anexo
+        estado_model = EstadoCohorteAnexo
+        return_url = 'cohortesAnexoIndex'
+    elif tipo_unidad_educativa == 'extension_aulica':
+        cohorte_unidad_educativa = CohorteExtensionAulica.objects.get(pk=cohorte_ue_id, extension_aulica__ambito__path__istartswith=request.get_perfil().ambito.path)
+        unidad_educativa = cohorte_unidad_educativa.extension_aulica
+        estado_model = EstadoCohorteExtensionAulica
+        return_url = 'cohortesExtensionAulicaIndex'
+
+    if request.method == 'POST':
+        if cohorte_unidad_educativa.estado.nombre != estado_model.FINALIZADA:
+            cohorte_unidad_educativa.estado = estado_model.objects.get(nombre=estado_model.FINALIZADA)
+            cohorte_unidad_educativa.save()
+            cohorte_unidad_educativa.registrar_estado()
+            request.set_flash('success', 'Se finalizó el seguimiento de la cohorte.')
+            return HttpResponseRedirect(reverse(return_url, args=[unidad_educativa.id]))
+        else:
+            request.set_flash('warning', 'La cohorte ya se encuentra finalizada.')
+            return HttpResponseRedirect(reverse(return_url, args=[unidad_educativa.id]))
+
+    return my_render(request, 'titulos/cohorte/seguimiento/finalizar_seguimiento.html', {
+        'cohorte_unidad_educativa': cohorte_unidad_educativa,
+        'unidad_educativa': unidad_educativa,
+        'tipo_unidad_educativa': tipo_unidad_educativa,
+    })
 
 @login_required
 #@credential_required('tit_cohorte_aceptar_asignacion')
